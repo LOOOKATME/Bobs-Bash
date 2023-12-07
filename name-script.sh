5949bfde5b2a66dd6033ad50d08da54c3d1c533c
Admin <admin@Admins-MacBook-Air.local>
2023-12-07 13:36:01 -0800

    name-script

---
+++ name-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Simple Bash Script
+
+# Prompt the user for their name
+echo "Enter your name:"
+read name
+
+# Print a greeting message
+echo "Hello, $name! Welcome to the Bash scripting world."
+
